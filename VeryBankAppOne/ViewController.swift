bc4da056b79108e2312347631c4c8579b6ad7726
Jeremy Mark <jermark851@gmail.com>
2020-03-30 16:36:23 -0500

    Initial Commit

---
+++ VeryBankAppOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeryBankAppOne
+//
+//  Created by Jeremy Mark on 3/30/20.
+//  Copyright © 2020 Jeremy Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
